f157514c795a30373c4f580e1c043d9a46262d6d
Fayd <fspea123@gmail.com>
2020-11-23 19:23:22 +1000

    scce

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 import numpy as np
 from tf_utils import CDFNet
 import nibabel as nib
+import matplotlib.pyplot as plt
 
 
 def segment(data, labels):
 
     #tf_data = tf.data.Dataset.from_tensor_slices(data)
     axial = CDFNet(num_filters=64, num_classes=2)
-    axial.compile(optimizer='adam', loss='binary_crossentropy')
+    axial.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
     #outputs = list()
     #for i in tf_data.batch(1):
@@ -40,7 +41,14 @@ def main():
 
     label = nib.load(DATA_FOLDER + 'ground.nii').get_fdata().astype(np.float32) / 255.
     label = np.moveaxis(label, -1, 0)
+
+    plt.imshow(label[20])
+    plt.show()
+
     label = tf.expand_dims(label, axis=-1)
+
+    plt.imshow(label.numpy()[20])
+    plt.show()
 
     segment(data, label)
 
