49180c122970bd50195328b7719c865b3e20d6db
Fayd <fspea123@gmail.com>
2020-11-25 20:00:52 +1000

    using validation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 import numpy as np
 from tf_utils import CDFNet
 import nibabel as nib
@@ -7,20 +8,34 @@ import nibabel as nib
 #DATA_FOLDER = '/home/fayd/Data/CHAOS'
 DATA_FOLDER = '/scratch/itee/uqfspear/organ-segmentation'
 
-def segment(data, labels):
+def segment(train, val):
+
+    x_train, y_train = train
+    x_val, y_val = val
 
     axial_model = CDFNet(num_filters=64, num_classes=6)
     axial_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
-    axial_model.fit(x=data, y=labels, batch_size=1, epochs=50)
-    preds = axial_model.predict(data)
+    # Callbacks
+    tensorboard_callback = TensorBoard(log_dir="./logs")
+    early_stopping_callback = EarlyStopping(restore_best_weights=True, patience=5)
+
+    # Train
+    axial_model.fit(x=x_train, y=y_train, batch_size=1, epochs=100, validation_data=(x_val, y_val),
+                    callbacks=[tensorboard_callback, early_stopping_callback])
 
     # Save model
     axial_model.save(DATA_FOLDER + '/model')
+    return axial_model
+
+
+def predict(model, data):
+
+    pred = model.predict(data)
 
     # Save image 3D array as nii
-    nii_label = nib.Nifti1Image(preds, affine=np.eye(4))
-    nii_label.to_filename(DATA_FOLDER + '/preds.nii')
+    nii_label = nib.Nifti1Image(pred, affine=np.eye(4))
+    nii_label.to_filename(DATA_FOLDER + '/pred.nii')
 
 
 def load_data(folders, type='InPhase'):
@@ -43,5 +58,7 @@ def load_data(folders, type='InPhase'):
 
 if __name__ == '__main__':
     print(tf.__version__)
-    x, y = load_data(['2', '3'])
-    segment(x, y)
+    train_data = load_data(['2', '3'])
+    val_data = load_data(['8'])
+    model = segment(train_data, val_data)
+    predict(model, val_data[0])
