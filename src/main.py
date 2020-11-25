fa069cabffcc2fcebd2b5f53ee81574b12f9866a
Fayd <fspea123@gmail.com>
2020-11-25 18:47:22 +1000

    update train script for wiener

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import nibabel as nib
 def segment(data, labels):
 
     #tf_data = tf.data.Dataset.from_tensor_slices(data)
-    axial = CDFNet(num_filters=64, num_classes=2)
+    axial = CDFNet(num_filters=64, num_classes=6)
     axial.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
     #outputs = list()
@@ -15,7 +15,7 @@ def segment(data, labels):
     #    outputs.append(axial(i))
     #outputs = tf.concat(outputs, axis=0)
 
-    axial.fit(x=data, y=labels, batch_size=1, epochs=1)
+    axial.fit(x=data, y=labels, batch_size=1, epochs=50)
     preds = axial.predict(data)
 
     # Save image 3D array as nii
@@ -27,23 +27,26 @@ def segment(data, labels):
 
 
 #DATA_FOLDER = '/home/fayd/Data/CHAOS_Converted_Train_Sets/CT/1/'
-#DATA_FOLDER = '/scratch/itee/uqfspear/organ-segmentation/'
+#DATA_FOLDER = '/home/fayd/Data/CHAOS/1'
+DATA_FOLDER = '/scratch/itee/uqfspear/organ-segmentation/1'
+
+def load_data(folder, type='InPhase'):
+
+    x = nib.load(folder + f'/{type}.nii').get_fdata().astype(np.float32) / 1000.
+    x = np.moveaxis(x, -1, 0)
+    if len(x.shape) < 4: x = tf.expand_dims(x, axis=-1)
+
+    y = nib.load(folder + '/ground.nii').get_fdata().astype(np.float32) / 63.
+    y = np.moveaxis(y, -1, 0)
+    y = tf.expand_dims(y, axis=-1)
+
+    return x, y
+
+
 
 def main():
-
-    #data = np.zeros((16, 64, 64, 1)).astype(np.float32)
-    #label = np.zeros((16, 64, 64, 3)).astype(np.float32)
-
-    data = nib.load(DATA_FOLDER + '4_.nii').get_fdata().astype(np.float32) / 1000.
-    data = np.moveaxis(data, -1, 0)
-    data = tf.expand_dims(data, axis=-1)
-
-    label = nib.load(DATA_FOLDER + 'ground.nii').get_fdata().astype(np.float32) / 255.
-    label = np.moveaxis(label, -1, 0)
-    label = tf.expand_dims(label, axis=-1)
-
-
-    segment(data, label)
+    x, y = load_data(DATA_FOLDER)
+    segment(x, y)
 
 
 if __name__ == '__main__':
