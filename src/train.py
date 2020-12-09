ec296f209ed752d9f9eaa31ed88bd17dcb1fcd58
Fayd <fspea123@gmail.com>
2020-12-10 06:47:18 +1000

    add test sample to show

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 import numpy as np
 import nibabel as nib
+import math
 
 from tf_utils.tfrecords import TFRecordsManager
 from tf_utils import misc
@@ -33,10 +34,57 @@ def train():
 
     # Fit model
     tensorboard_callback = TensorBoard(log_dir="./logs")
-    model.fit(train_dataset, epochs=10, callbacks=[tensorboard_callback], validation_data=val_dataset)
+    model.fit(train_dataset, epochs=3, callbacks=[tensorboard_callback], validation_data=val_dataset)
 
     # Save model
     model.save(base_path + '/model')
+
+    test = load_data(['2'])
+    predict(model, test[0])
+
+
+
+def predict(model, data):
+
+    base_path = misc.get_base_path(training=False)
+
+    labels = model.predict(data)
+
+    labels = np.swapaxes(labels, 0, 2)
+    labels = np.swapaxes(labels, 0, 1)
+
+    # Collapse the probabilities into a one hot encoding
+    labels = np.eye(labels.shape[-1])[labels.argmax(axis=-1)]
+
+    # Multiply out and sum
+    num_classes = labels.shape[-1]
+    labels *= [i * math.floor(255. / num_classes) for i in range(num_classes)]
+    labels = np.sum(labels, axis=-1)
+
+    # Save image 3D array as nii
+    nii_label = nib.Nifti1Image(labels, affine=np.eye(4))
+    nii_label.to_filename(base_path + '/fixed_preds.nii')
+
+
+def load_data(folders, type='Combined'):
+
+    base_path = misc.get_base_path(training=False)
+
+    x, y = list(), list()
+    for folder in folders:
+
+        a = nib.load(base_path + f'/{folder}/{type}.nii').get_fdata().astype(np.float32) / 1000.
+        a = np.moveaxis(a, -2, 0)
+        print(a.shape)
+        if len(a.shape) < 4: a = tf.expand_dims(a, axis=-1)
+        x.append(a)
+
+        b = nib.load(base_path + f'/{folder}/ground.nii').get_fdata().astype(np.float32) / 63.
+        b = np.moveaxis(b, -1, 0)
+        b = tf.expand_dims(b, axis=-1)
+        y.append(b)
+
+    return tf.concat(x, axis=0), tf.concat(y, axis=0)
 
 
 if __name__ == '__main__':
