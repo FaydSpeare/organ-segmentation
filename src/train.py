84ad0373eff6137fe49d82f456cb186e6bcea7cc
Fayd <fspea123@gmail.com>
2020-12-12 21:17:55 +1000

    FIXED BUG AGAIN

--- src/train.py
+++ src/train.py
@@ -39,11 +39,11 @@ def train():
 
     # Fit model
     tensorboard_callback = TensorBoard(log_dir="./logs")
-    early_stopping_callback = EarlyStopping(restore_best_weights=True, patience=3)
+    #early_stopping_callback = EarlyStopping(restore_best_weights=True, patience=3)
     #model.fit(train_dataset, epochs=100, callbacks=[tensorboard_callback],
     #          validation_data=val_dataset, class_weight={0: 270., 1: 11., 2: 1., 3:1., 4:1.})
 
-    model.fit(train_dataset, epochs=100, callbacks=[tensorboard_callback, early_stopping_callback], validation_data=val_dataset)
+    model.fit(train_dataset, epochs=100, callbacks=[tensorboard_callback], validation_data=val_dataset)
 
     # Save model
     model.save(base_path + '/model')
