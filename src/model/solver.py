fbbf11eb64ae8f90c3a244a66a38a0e4c2ebe5aa
Fayd <fspea123@gmail.com>
2020-12-13 15:53:52 +1000

    update softmax in loss functions

--- src/model/solver.py
+++ src/model/solver.py
@@ -29,13 +29,12 @@ class SegSolver:
 
         for batch in dataset:
             y = tf.keras.utils.to_categorical(tf.cast(batch['Y'], tf.int32), num_classes=self.params['out_channels'])
-            training = True if mode == 'train' else False
-            predictions, metrics = self.step(batch['X'], y, training=training)
+            predictions, metrics = self.step(batch['X'], y, training=(mode == 'train'))
             self.tb.update_metrics(metrics)
 
         self.tb.write_summary(mode, epoch)
 
-    @tf.function
+    #@tf.function
     def step(self, x, y, training=True):
 
         loss_fn = self.mm.metrics[self.params['loss_fn']]
@@ -44,17 +43,17 @@ class SegSolver:
 
             with tf.GradientTape() as tape:
                 logits = self.network(x, training=True)
-                loss = loss_fn(y, logits)
+                loss = loss_fn(y, logits, probs=True)
 
             gradients = tape.gradient(loss, self.network.trainable_variables)
-            scores_by_class = self.mm.dice_score_from_logits(y, logits)
+            scores_by_class = self.mm.dice_score_from_logits(y, logits, probs=True)
             self.optimizer.apply_gradients(zip(gradients, self.network.trainable_variables))
 
         else:
 
             logits = self.network(x, training=False)
-            scores_by_class = self.mm.dice_score_from_logits(y, logits)
-            loss = loss_fn(y, logits)
+            scores_by_class = self.mm.dice_score_from_logits(y, logits, probs=True)
+            loss = loss_fn(y, logits, probs=True)
 
         return misc.get_argmax_prediction(logits), {'loss' : loss, 'scores_by_class' : scores_by_class}
 
