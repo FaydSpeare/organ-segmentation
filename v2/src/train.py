f4c018e36b36b973e94a7e2dadd68971d31ef10e
Fayd <fspea123@gmail.com>
2021-01-29 19:55:47 +1000

    remove prediction from train script

--- v2/src/train.py
+++ v2/src/train.py
@@ -1,8 +1,3 @@
-import numpy as np
-import nibabel as nib
-import tensorflow as tf
-import math
-
 from common.tfrecords import TFRecordsManager
 from model.solver import Solver
 from model.network import CDFNet
