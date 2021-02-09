bd01e2e664cb8026ca354f62cb425db61b700d36
Fayd <fspea123@gmail.com>
2021-02-10 01:20:22 +1000

    prediction fix

--- v2/src/train.py
+++ v2/src/train.py
@@ -45,7 +45,7 @@ def main():
         if solver.early_stopping_tick > parameters[p.PATIENCE]:
             break
 
-    if type(parameters[p.NETWORK]) == CDFNet:
+    if type(parameters[p.NETWORK]()) == CDFNet:
         # Run predictions for dataset
         model_folder = parameters[p.MODEL_PATH].split('/')[-2]
         predict(model_folder, parameters[p.TFRECORDS], prefix=parameters[p.PREFIX])
